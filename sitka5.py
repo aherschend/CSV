fe60ee7f97cc3abbb12a858d5283c2a270da59a3
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-10-05 23:18:34 -0500

    2 graphs

--- sitka5.py
+++ sitka5.py
@@ -17,7 +17,10 @@ dates = []
 highs = []
 lows = []
 
+
+# how do we define date_index here that way we aren't hard-coding it?
 for row in reader:
+    #current_date = datetime.strptime(row[date_index], "%Y-%m-%d")
     try: 
         the_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
@@ -50,5 +53,46 @@ plt.show()
 # for Death Valley
 
 open_file2 = open("death_valley_2018_simple.csv","r")
+
+
+csv_file2 = csv.reader(open_file2, delimiter = ",")
+
+reader2 = csv.reader(open_file2)
+header_row2 = next(reader2)
+
+dates2 = []
+highs2 = []
+lows2 =[]
+
+
+for row in reader2:
+    try:
+        the_date2 = datetime.strptime(row[2], '%Y-%m-%d')
+        high2 = int(row[4])
+        low2 = int(row[5])
+    except ValueError:
+        print(f"Missing data for {the_date2}")
+    else:
+        dates2.append(the_date2)
+        highs2.append(int(row[4]))
+        lows2.append(int(row[5]))
+
+fig2 = plt.figure()
+
+plt.title("DEATH VALLEY, CA US")
+plt.xlabel("", fontsize = 12)
+plt.ylabel("Temperature (F)", fontsize = 12)
+plt.tick_params(axis = "both", which = "major", labelsize = 12)
+
+plt.plot(dates2, highs2, c = "red")
+plt.plot(dates2, lows2, c = "blue")
+
+plt.fill_between(dates2, highs2, lows2, facecolor = "blue", alpha = 0.1)
+
+
+fig2.autofmt_xdate()
+
+plt.show()
+
 
 
